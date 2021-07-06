6037889890678937dd2579b5d3e6b9bd212f842a
OGdeveloper-AI <83118799+OGdeveloper-AI@users.noreply.github.com>
2021-07-07 01:19:50 +0400

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,5 @@
+Radius = float(input('What is the radius of the circle? '))
+Area = 3.14159 * Radius ** 2
+print("The radius of the circle : " , Radius)
+print("The area of the circle : " , Area)
+
---
+++ Assignment 2.py
@@ -0,0 +1,4 @@
+filename = input("Input the Filename: ")               
+file = filename.split('.')
+extension = file[-1]
+print('The extension of the file is :', extension ) 
